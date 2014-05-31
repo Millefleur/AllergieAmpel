6007212e2ced933f42d3a5b71080ca080e44864c
Sandra S <sandra.steinitz@hotmail.com>
2014-05-31 18:34:44 +0200

    added git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AllergieAmpel::Application.config.secret_key_base = 'bcfce919658b83a12cf454a6bb247c14c7c7ba22265e11b318af6034e0ac6327c4f91ab94ad2bc0c01ba386382fe93ab13194e193a4d88746d0870b2ac830aeb'
